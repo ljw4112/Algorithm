559c911dac962ec8257d889e965fad1a5b28d469
ljw4112 <silimy5465@gmail.com>
2021-02-15 16:49:36 +0900

    백준 온라인 저지

--- Algorithm/1316.cpp
+++ Algorithm/1316.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	
+	int x;
+	cin >> x;
+
+	int num = 0;
+	while (x--) {
+		bool ch[26] = { false };
+		string s;
+		cin >> s;
+
+		bool group = true;
+		for (int i = 0; i < s.length(); i++) {
+			if (!ch[s[i] - 97]) {
+				ch[s[i] - 97] = true;
+				continue;
+			}
+			if (ch[s[i] - 97] && i != 0 && s[i-1] != s[i]) {
+				group = false;
+				break;
+			}
+		}
+
+		if (group)
+			num++;
+	}
+
+	cout << num << endl;
 }
